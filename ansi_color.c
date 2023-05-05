d5dc66443303573a9548f8fc05795862560d74b2
enso02 <ensar.baran@stud.thga.de>
2023-05-05 22:01:38 +0200

    Datei ansi_color.c hinzugefügt

---
+++ ansi_color.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+  printf("\033[33m"); // sonne in gelb
+  printf("    \   /    \n");
+  printf("     .-.     \n");
+  printf("  --(   )--  \n");
+  printf("     `-’     \n");
+  printf("    /   \    \n");
+  printf("\033[0m"); 
+
+  return 0;
+}
+

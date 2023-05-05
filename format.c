0dc6e2c0967f0c49642a55ede661c038e2dec91a
RamizYasar <ramiz.yasar@stud.thga.de>
2023-05-05 21:20:47 +0200

    format specifier code hinzugefügt

---
+++ format.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int ganzz = 40;
+  float gleitkommz = 5.2567;
+  char character = 'r';
+  char str[] = "Hello world";
+  
+  printf("Ganzzahl: %i\n", ganzz);
+  printf("Gleitkommazahl: %f\n", gleitkommaz);
+  printf("Character: %c\n", character);
+  printf("String: %s\n", str);
+  
+  return 0;
+}
